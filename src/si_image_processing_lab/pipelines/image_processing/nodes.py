c5cf13f5e9cbac9253282f5a519ab90752530ba7
Josep <ponceespejojoserenato@gmail.com>
2026-06-06 04:57:19 -0500

    Fix output directory creation

--- src/si_image_processing_lab/pipelines/image_processing/nodes.py
+++ src/si_image_processing_lab/pipelines/image_processing/nodes.py
@@ -1,10 +1,17 @@
-from PIL import Image,ImageFilter,ImageDraw
+from PIL import Image, ImageFilter, ImageDraw
+import os
 
-def process_image(input_path:str,output_path:str):
+def process_image(input_path: str, output_path: str):
+
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+
     image = Image.open(input_path)
     rotated = image.rotate(45)
     filtered = rotated.filter(ImageFilter.EMBOSS)
+
     draw = ImageDraw.Draw(filtered)
-    draw.text((20,20),"UCV - Sistemas Inteligentes",fill="white")
+    draw.text((20, 20), "UCV - Sistemas Inteligentes", fill="white")
+
     filtered.save(output_path)
+
     return output_path
